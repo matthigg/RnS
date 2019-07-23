d7f7f92205ac564e7dcbfb9ed58f56dd653e8db1
matthigg <mhh129@gmail.com>
2019-07-23 16:50:12 -0400

    Update DEBUG in settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['RNS_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['RNS_DEBUG']
+DEBUG = os.environ['RNS_DEBUG'] == 1
 
 ALLOWED_HOSTS = ['*']
 
