cc648a1cfed46575d3da2291ec044a2b037a228a
matthigg <mhh129@gmail.com>
2019-07-23 18:31:04 -0400

    Add Postgres to DATABASES{}

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+
+    # SQLite3
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    # Postgres
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'ENGINE':   'django.db.backends.postgresql',
+      'NAME':     os.environ['RNS_DATABASE_NAME'],
+      'USER':     os.environ['RNS_DATABASE_USER'],
+      'PASSWORD': os.environ['RNS_DATABASE_PASSWORD'],
+      'HOST':     os.environ['RNS_DATABASE_HOST'],
+      'PORT':     os.environ['RNS_DATABASE_PORT'],
     }
 }
 
