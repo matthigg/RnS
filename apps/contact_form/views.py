9df6be898fc1cdb31c4eea3fed9b554ebd429db8
matthigg <mhh129@gmail.com>
2019-07-26 19:24:24 -0400

    Add info and error message templating to contact-form.html

--- apps/contact_form/views.py
+++ apps/contact_form/views.py
@@ -8,28 +8,28 @@ import requests
 def submit(request):
 
   # reCAPTCHA v3
-  r = requests.post(
-    'https://www.google.com/recaptcha/api/siteverify', 
-    params={
-      'response': request.POST["g-recaptcha-response"],
-      'secret': os.environ["RECAPTCHA_SECRET_KEY"],
-    }
-  )
+  # r = requests.post(
+  #   'https://www.google.com/recaptcha/api/siteverify', 
+  #   params={
+  #     'response': request.POST["g-recaptcha-response"],
+  #     'secret': os.environ["RECAPTCHA_SECRET_KEY"],
+  #   }
+  # )
 
   # Return 'error' message if reCAPTCHA fails
-  if r.json()['success'] == False:
-      messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
-      return redirect('contact')
+  # if r.json()['success'] == False:
+  #     messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
+  #     return redirect('contact')
 
   if request.method == "POST":
     form = CreateContactForm(request.POST)
     if form.is_valid():
       form.save()
 
-      messages.add_message(request, messages.INFO, 'Form is valid!')
+      messages.add_message(request, messages.INFO, 'Your Message Has Been Sent!')
       return redirect('thanks')
 
     # Django stores errors in form.error
     else:
       form = CreateContactForm()
-    return render(request, 'contact', { 'form': form })
+    return render(request, 'contact.html', { 'form': form })
