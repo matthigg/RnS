d2d62f5d923e869194afd78287542e779a69997c
matthigg <mhh129@gmail.com>
2019-07-26 20:37:08 -0400

    Update thanks.html UI

--- apps/contact_form/views.py
+++ apps/contact_form/views.py
@@ -8,19 +8,20 @@ import requests
 def submit(request):
 
   # reCAPTCHA v3
-  # r = requests.post(
-  #   'https://www.google.com/recaptcha/api/siteverify', 
-  #   params={
-  #     'response': request.POST["g-recaptcha-response"],
-  #     'secret': os.environ["RECAPTCHA_SECRET_KEY"],
-  #   }
-  # )
+  r = requests.post(
+    'https://www.google.com/recaptcha/api/siteverify', 
+    params={
+      'response': request.POST["g-recaptcha-response"],
+      'secret': os.environ["RECAPTCHA_SECRET_KEY"],
+    }
+  )
 
   # Return 'error' message if reCAPTCHA fails
-  # if r.json()['success'] == False:
-  #     messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
-  #     return redirect('contact')
+  if r.json()['success'] == False:
+    messages.add_message(request, messages.INFO, 'There was an error with your request. Please contact us at (123) 456-7890 or email us at email@email.com.')
+    return redirect('contact')
 
+  # Save message to database if valid
   if request.method == "POST":
     form = CreateContactForm(request.POST)
     if form.is_valid():
