fcde737463cc8c7c27f7289be5996f6657dc6335
matthigg <mhh129@gmail.com>
2019-09-22 17:50:53 -0400

    Trying to add db.sqlite3

--- config/settings.py
+++ config/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
 
     # SQLite3
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
     # Postgres
     # 'default': {
