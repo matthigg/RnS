bcc82cb4963c53f4d0289a9ee6ea950ce4e61c5c
matthigg <mhh129@gmail.com>
2019-09-24 10:39:11 -0400

    Update free quote and call button-related css and js files

--- local_static/js/base.js
+++ local_static/js/base.js
@@ -1,65 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
   
   const navbar = document.querySelector('.navbar');
-  // const navbar_brand = document.querySelector('.navbar-brand');
-  const navbar_free_quote = document.querySelector('.navbar-free-quote');
+  const nav_free_estimate = document.querySelector('.nav-free-estimate');
 
+  // Change the appearance of the navbar when the user scrolls down the page
   window.addEventListener('scroll', () => {
-
-    // Shrink Navbar on Page Scroll
-    //
-    // Change the appearance of the navbar when the user scrolls down the page
     if (window.scrollY > 1) {
 
       // Shrink the navbar
       navbar.classList.add('navbar-minimized-true');
       navbar.classList.remove('navbar-minimized-false');
 
-      // Shrink the navbar brand/logo
-      // navbar_brand.classList.add('shrink');
-      // navbar_brand.classList.remove('expand');
-
-      // Reposition/move up the 'free quote' button
-      navbar_free_quote.classList.add('navbar-free-quote-minimized-true');
-      navbar_free_quote.classList.remove('navbar-free-quote-minimized-false');
+      // Reposition/move up the 'free estimate' button
+      nav_free_estimate.classList.add('nav-free-quote-minimized');
+      nav_free_estimate.classList.remove('nav-free-quote-maximized');
     } else {
 
       // Expand the navbar
       navbar.classList.add('navbar-minimized-false');
       navbar.classList.remove('navbar-minimized-true');
 
-      // Shrink the navbar brand/logo
-      // navbar_brand.classList.add('expand');
-      // navbar_brand.classList.remove('shrink');
-
-      // Reposition/move down the 'free quote' button
-      navbar_free_quote.classList.add('navbar-free-quote-minimized-false');
-      navbar_free_quote.classList.remove('navbar-free-quote-minimized-true');
+      // Reposition/move down the 'free estimate' button
+      nav_free_estimate.classList.add('nav-free-quote-maximized');
+      nav_free_estimate.classList.remove('nav-free-quote-minimized');
     }
   })
 
-  // Underline Active Navbar Links & Toggle Free Quote Button Visibility
+  // Underline Active Navbar Links & Toggle Free Estimate Button Visibility
   //
   // Assign the .active class to the current page's nav-link which decorates it
   // with an underscore (to give an indication which page is the current page)
-  // & display the 'Free Quote' button -not- on contact.html or thanks.html.
+  // & show the 'Free Estimate' button in index, services, our work, and about
+  // pages.
   if (document.querySelector('.container-fluid').classList.contains('index-page')) {
     document.querySelector('.nav-link-home').classList.add('active');
-    document.querySelector('.navbar-free-quote').style.display = 'block';
+    document.querySelector('.nav-free-estimate').style.display = 'block';
   } else if (document.querySelector('.container-fluid').classList.contains('services-page')) {
     document.querySelector('.nav-link-services').classList.add('active');
-    document.querySelector('.navbar-free-quote').style.display = 'block';
+    document.querySelector('.nav-free-estimate').style.display = 'block';
   } else if (document.querySelector('.container-fluid').classList.contains('our-work-page')) {
     document.querySelector('.nav-link-our-work').classList.add('active');
-    document.querySelector('.navbar-free-quote').style.display = 'block';
+    document.querySelector('.nav-free-estimate').style.display = 'block';
   } else if (document.querySelector('.container-fluid').classList.contains('about-page')) {
     document.querySelector('.nav-link-about').classList.add('active');
-    document.querySelector('.navbar-free-quote').style.display = 'block';
+    document.querySelector('.nav-free-estimate').style.display = 'block';
   } else if (document.querySelector('.container-fluid').classList.contains('contact-page')) {
     document.querySelector('.nav-link-contact').classList.add('active');
   }
 
-  // Contact Form 
+  // Contact Form Required Fields
   //
   // Add an asterisk * next to each contact form field that is a required field
   if (document.querySelector('.contact-form') !== null) {
@@ -127,4 +116,28 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(single_src)
     document.querySelector('.modal-single-image-element').src = single_src;
   }
+
+  // User clicked 'Call Now' button -- call phone number
+  document.querySelector('.nav-call-now').addEventListener('click', () => {
+    gtag('event', 'click_call_now_button', {
+      'event_category': 'engagement',
+      'event_label': 'User clicked the call button to make a phone call',
+      'value': '50',
+      'event_callback': function() {
+        window.open('tel:+1-804-258-0403', '_self')
+      },
+    });
+  })
+
+  // User clicked 'FREE Estimate' button -- go to contact page
+  document.querySelector('.nav-free-estimate').addEventListener('click', (event) => {
+    gtag('event', 'click_free_quote_button', {
+      'event_category': 'engagement',
+      'event_label': 'User clicked the free quote button to go to the Contact Us page',
+      'value': '0',
+      'event_callback': function() {
+        window.open(window.location.origin + event.target.dataset.url, '_self')
+      },
+    });
+  })
 })
