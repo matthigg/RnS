49b8dd7c6cb9944d901c7fd8c9431d153c95285a
matthigg <mhh129@gmail.com>
2019-10-01 19:11:24 -0400

    Update sitemap

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -2,11 +2,12 @@ from django.contrib import sitemaps
 from django.urls import reverse
 
 class StaticViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
-    changefreq = 'daily'
+  priority = 0.5
+  changefreq = 'daily'
 
-    def items(self):
-        return ['', 'about', 'contact', 'our-work', 'services', 'fence-cleaning', 'surface-cleaning', 'soft-wash', 'outdoor-stain-removal', 'deck-cleaning-restoration']
+  def items(self):
+    # return ['index', 'about', 'contact', 'our-work', 'services', 'fence-cleaning', 'surface-cleaning', 'soft-wash', 'outdoor-stain-removal', 'deck-cleaning-restoration']
+    return ['about']
 
-    def location(self, item):
-        return reverse(item)
+  def location(self, item):
+    return reverse(item)
