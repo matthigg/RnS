dadb756e72f153489470241f0212960afdb0739a
matthigg <mhh129@gmail.com>
2019-10-01 21:26:58 -0400

    Update sitemap

--- config/sitemaps.py
+++ config/sitemaps.py
@@ -6,8 +6,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
   changefreq = 'daily'
 
   def items(self):
-    # return ['index', 'about', 'contact', 'our-work', 'services', 'fence-cleaning', 'surface-cleaning', 'soft-wash', 'outdoor-stain-removal', 'deck-cleaning-restoration']
-    return ['about']
+    return ['index', 'about', 'contact', 'our-work', 'services', 'fence-cleaning', 'surface-cleaning', 'soft-wash', 'outdoor-stain-removal', 'deck-cleaning-restoration']
 
   def location(self, item):
     return reverse(item)
